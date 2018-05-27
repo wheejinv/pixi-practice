52147cbb7ffc62ee3e45da212d4ff501d4e00d92
whee <wheejinv@naver.com>
2018-05-28 02:51:24 +0900

    router & render init

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const router = require('./router');
 const app = express();
 
+app.use(express.static('public'));
+// app.use(express.static('pixi'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+router(app);
 
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
-app.listen( 4040, () => console.log('Example app listening on port 3000!'));
+app.listen( 4040, () => console.log('Pixi-Practice app listening on port 4040!'));
