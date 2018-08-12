b6120c334a4975814c92ecea8db9c0815483c7fc
whee <wheejinv@naver.com>
2018-08-12 12:10:27 +0900

    inherit logic improve -> subClass has closure F

--- src/vanilla/inheritTest.js
+++ src/vanilla/inheritTest.js
@@ -1,33 +1,39 @@
-function subClass( obj ) {
-    var parent = this === window ? Function : this;
+var subClass = (function subClass() {
     var F = function() {};
 
-    var child = function() {
-        var _parent = child.parent;
+    var subClass = function( obj ) {
+        var parent = this === window ? Function : this;
 
-        if( _parent && _parent !== Function ) {
-            _parent.apply( this, arguments )
+
+        var child = function() {
+            var _parent = child.parent;
+
+            if( _parent && _parent !== Function ) {
+                _parent.apply( this, arguments )
+            }
+
+            if( child.prototype.ctor ) {
+                child.prototype.ctor.apply( this, arguments );
+            }
+        };
+
+        F.prototype = parent.prototype;
+        child.prototype = new F();
+        child.prototype.constructor = child;
+        child.parent = parent;
+        child.subClass = arguments.callee;
+
+        for( var i in obj ) {
+            if( obj.hasOwnProperty(i)) {
+                child.prototype[ i ] = obj[ i ];
+            }
         }
 
-        if( child.prototype.ctor ) {
-            child.prototype.ctor.apply( this, arguments );
-        }
+        return child;
     };
 
-    F.prototype = parent.prototype;
-    child.prototype = new F();
-    child.prototype.constructor = child;
-    child.parent = parent;
-    child.subClass = arguments.callee;
-
-    for( var i in obj ) {
-        if( obj.hasOwnProperty(i)) {
-            child.prototype[ i ] = obj[ i ];
-        }
-    }
-
-    return child;
-}
+    return subClass;
+})();
 
 var person_obj = {
     ctor: function() {
@@ -44,13 +50,23 @@ var person_obj = {
 
 var student_obj = {
     ctor: function() {
-        console.log( "person init" );
+        console.log( "student init" );
     },
 
     getName: function() {
         return "Student Name: " + this._name;
     }
 };
+
+var highStudent_obj = {
+    ctor: function() {
+        console.log( "high init");
+    },
+
+    getName: function() {
+        return "High Name: " + this._name;
+    }
+}
 
 var Person = subClass( person_obj ); // Person 클래스 정의
 var person = new Person();
@@ -62,4 +78,9 @@ var student = new Student();
 student.setName("iamjoo");
 console.log( student.getName() );
 
-console.log( Person.toString() );
+var High = Student.subClass( highStudent_obj );
+var high = new High();
+high.setName("ss");
+console.log( high.getName() );
+
+// console.log( Person.toString() );
